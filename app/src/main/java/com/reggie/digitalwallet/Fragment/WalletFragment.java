b8e0f22ef26f6340e99148d1049e73f83bd1bd63
reggie1996 <240730853@qq.com>
2018-01-15 11:07:36 +0800

    拍照相册

--- app/src/main/java/com/reggie/digitalwallet/Fragment/WalletFragment.java
+++ app/src/main/java/com/reggie/digitalwallet/Fragment/WalletFragment.java
@@ -2,10 +2,14 @@ package com.reggie.digitalwallet.Fragment;
 
 
 import android.content.Context;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.OnApplyWindowInsetsListener;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.WindowInsetsCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -15,34 +19,160 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.reggie.digitalwallet.Activity.MainActivity;
 import com.reggie.digitalwallet.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import de.mrapp.android.tabswitcher.Layout;
+import de.mrapp.android.tabswitcher.PullDownGesture;
+import de.mrapp.android.tabswitcher.SwipeGesture;
 import de.mrapp.android.tabswitcher.Tab;
 import de.mrapp.android.tabswitcher.TabSwitcher;
 import de.mrapp.android.tabswitcher.TabSwitcherDecorator;
+import de.mrapp.android.util.ThemeUtil;
+
+import static de.mrapp.android.util.DisplayUtil.getDisplayWidth;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class WalletFragment extends BaseFragment {
 
+    private TabSwitcher tabSwitcher;
+
+    private static final String VIEW_TYPE_EXTRA = WalletFragment.class.getName() + "::ViewType";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wallet, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_wallet, container, false);
+        tabSwitcher = view.findViewById(R.id.tab_switcher);
+        ViewCompat.setOnApplyWindowInsetsListener(tabSwitcher, createWindowInsetsListener());
+        tabSwitcher.setDecorator(new Decorator());
+        for (int i = 0; i < 5; i++) {
+            tabSwitcher.addTab(createTab(i));
+        }
+
+
+        return view;
     }
 
     @Override
     protected View initView() {
+
+
         return null;
     }
 
     @Override
     protected void initData() {
     }
+
+
+
+    private class Decorator extends TabSwitcherDecorator {
+
+        @NonNull
+        @Override
+        public View onInflateView(@NonNull final LayoutInflater inflater,
+                                  @Nullable final ViewGroup parent, final int viewType) {
+            View view;
+
+            if (viewType == 0) {
+                view = inflater.inflate(R.layout.tab_edit_text, parent, false);
+            } else {
+                view = inflater.inflate(R.layout.tab_text_view, parent, false);
+            }
+            /*
+            Toolbar toolbar = view.findViewById(R.id.toolbar);
+            toolbar.inflateMenu(R.menu.tab);
+            toolbar.setOnMenuItemClickListener(createToolbarMenuListener());
+            Menu menu = toolbar.getMenu();
+            TabSwitcher.setupWithMenu(tabSwitcher, menu, createTabSwitcherButtonListener());
+            */
+            return view;
+        }
+
+        @Override
+        public void onShowTab(@NonNull final Context context,
+                              @NonNull final TabSwitcher tabSwitcher, @NonNull final View view,
+                              @NonNull final Tab tab, final int index, final int viewType,
+                              @Nullable final Bundle savedInstanceState) {
+  //          TextView textView = findViewById(android.R.id.title);
+//            textView.setText(tab.getTitle());
+
+            //Toolbar toolbar = findViewById(R.id.toolbar);
+            //toolbar.setVisibility(tabSwitcher.isSwitcherShown() ? View.GONE : View.VISIBLE);
+
+            if (viewType != 0) {
+                EditText editText = findViewById(android.R.id.edit);
+
+                if (savedInstanceState == null) {
+                    editText.setText(null);
+                }
+
+                editText.requestFocus();
+            }
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 2;
+        }
+
+        @Override
+        public int getViewType(@NonNull final Tab tab, final int index) {
+            Bundle parameters = tab.getParameters();
+            return parameters != null ? parameters.getInt("1") : 0;
+        }
+
+    }
+
+    @NonNull
+    private Tab createTab(final int index) {
+        CharSequence title = getString(R.string.tab_title, index + 1);
+        Tab tab = new Tab(title);
+        Bundle parameters = new Bundle();
+        parameters.putInt(VIEW_TYPE_EXTRA, index % 2);
+        tab.setParameters(parameters);
+        return tab;
+    }
+
+    @NonNull
+    private OnApplyWindowInsetsListener createWindowInsetsListener() {
+        return new OnApplyWindowInsetsListener() {
+
+            @Override
+            public WindowInsetsCompat onApplyWindowInsets(final View v,
+                                                          final WindowInsetsCompat insets) {
+                int left = insets.getSystemWindowInsetLeft();
+                int top = insets.getSystemWindowInsetTop();
+                int right = insets.getSystemWindowInsetRight();
+                int bottom = insets.getSystemWindowInsetBottom();
+                tabSwitcher.setPadding(left, top, right, bottom);
+                float touchableAreaTop = top;
+
+                if (tabSwitcher.getLayout() == Layout.TABLET) {
+                    touchableAreaTop += getResources()
+                            .getDimensionPixelSize(R.dimen.tablet_tab_container_height);
+                }
+
+                RectF touchableArea = new RectF(left, touchableAreaTop,
+                        getDisplayWidth(mContext) - right, touchableAreaTop +
+                        ThemeUtil.getDimensionPixelSize(mContext, R.attr.actionBarSize));
+                tabSwitcher.addDragGesture(
+                        new SwipeGesture.Builder().setTouchableArea(touchableArea).create());
+                tabSwitcher.addDragGesture(
+                        new PullDownGesture.Builder().setTouchableArea(touchableArea).create());
+                return insets;
+            }
+
+        };
+    }
+
 
 }
--- app/src/main/java/com/reggie/digitalwallet/Test/TestActivity.kt
+++ app/src/main/java/com/reggie/digitalwallet/Test/TestActivity.kt
@@ -1,20 +1,85 @@
 package com.reggie.digitalwallet.Test
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.jph.takephoto.app.TakePhoto
+import com.jph.takephoto.app.TakePhotoActivity
+import com.jph.takephoto.model.InvokeParam
+import com.jph.takephoto.model.TResult
+import com.jph.takephoto.permission.InvokeListener
+import com.jph.takephoto.permission.PermissionManager
 import com.reggie.digitalwallet.Activity.MainActivity
 import com.reggie.digitalwallet.Activity.QRCodeActivity
 import com.reggie.digitalwallet.R
 import com.reggie.digitalwallet.Utils.QRCodeGenerator
 import kotlinx.android.synthetic.main.activity_test.*
+import com.jph.takephoto.app.TakePhotoImpl
+import com.jph.takephoto.permission.TakePhotoInvocationHandler
+import com.jph.takephoto.permission.PermissionManager.TPermissionType
+import com.jph.takephoto.model.TContextWrap
 
-class TestActivity : Activity() {
+
+
+
+
+class TestActivity : Activity() , TakePhoto.TakeResultListener, InvokeListener {
+
+    private val TAG = TakePhotoActivity::class.java.name
+    private var takePhoto: TakePhoto? = null
+    private var invokeParam: InvokeParam? = null
+
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        val type = PermissionManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        PermissionManager.handlePermissionsResult(this, type, invokeParam, this)
+    }
+
+    override fun takeSuccess(result: TResult?) {
+        val path = result?.getImage()?.getCompressPath()
+        Toast.makeText(this, path, Toast.LENGTH_LONG).show()
+        //Log.i(TAG,"takeSuccess：" + result?.getImage()?.getCompressPath());
+    }
+
+    override fun takeCancel() {
+        Toast.makeText(this, "取消", Toast.LENGTH_LONG).show()
+    }
+
+    override fun takeFail(result: TResult?, msg: String?) {
+        Toast.makeText(this, "失败" + msg, Toast.LENGTH_LONG).show()
+    }
+
+    override fun invoke(invokeParam: InvokeParam?): PermissionManager.TPermissionType {
+        val type = PermissionManager.checkPermission(TContextWrap.of(this), invokeParam!!.getMethod())
+        if (TPermissionType.WAIT == type) {
+            this.invokeParam = invokeParam
+        }
+        return type
+    }
+
+    /**
+     *
+     * 获取TakePhoto实例
+     *
+     * @return
+     */
+
+    fun getTakePhoto(): TakePhoto? {
+        if (takePhoto == null) {
+            takePhoto = TakePhotoInvocationHandler.of(this).bind(TakePhotoImpl(this, this)) as TakePhoto
+        }
+        return takePhoto
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        getTakePhoto()?.onCreate(savedInstanceState)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
         buttonmain.setOnClickListener{startActivity(Intent(this, MainActivity::class.java))}
@@ -25,12 +90,20 @@ class TestActivity : Activity() {
             Glide.with(this).load(bitmap).into(qrcode)
         };
         buttonsfz.setOnClickListener { startActivity(Intent(this, SFZActivity::class.java)) }
+
+        buttonchoosephoto.setOnClickListener{ takePhoto?.onPickFromDocuments()}
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        getTakePhoto()?.onSaveInstanceState(outState)
+        super.onSaveInstanceState(outState)
     }
 
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        getTakePhoto()?.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data)
-
         when(requestCode){
             1 ->{
                 Toast.makeText(this,data?.getStringExtra("result"),Toast.LENGTH_LONG).show()
