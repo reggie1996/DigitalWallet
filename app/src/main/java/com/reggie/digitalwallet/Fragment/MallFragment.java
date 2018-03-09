0a4641561b748ae5d349a6d65f7542ad00513fa8
reggie1996 <240730853@qq.com>
2018-03-09 12:33:18 +0800

    3/9

--- app/src/main/java/com/reggie/digitalwallet/Fragment/MallFragment.java
+++ app/src/main/java/com/reggie/digitalwallet/Fragment/MallFragment.java
@@ -6,13 +6,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.reggie.digitalwallet.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MallFragment extends BaseFragment {
+
+
+    ImageView iv_mall1;
+    ImageView iv_mall2;
+    ImageView iv_mall3;
+    ImageView iv_store1;
+    ImageView iv_store2;
+    ImageView iv_store3;
+    ImageView iv_store4;
+    ImageView iv_store5;
+    private View view;
 
     @Override
     protected View initView() {
@@ -23,8 +36,28 @@ public class MallFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mall, container, false);
+        view = inflater.inflate(R.layout.fragment_mall, container, false);
+        initImage();
+        return view;
     }
 
+    private void initImage() {
+        iv_mall1 = view.findViewById(R.id.iv_mall1);
+        iv_mall2 = view.findViewById(R.id.iv_mall2);
+        iv_mall3 = view.findViewById(R.id.iv_mall3);
+        iv_store1 = view.findViewById(R.id.iv_store1);
+        iv_store2 = view.findViewById(R.id.iv_store2);
+        iv_store3 = view.findViewById(R.id.iv_store3);
+        iv_store4 = view.findViewById(R.id.iv_store4);
+        iv_store5 = view.findViewById(R.id.iv_store5);
+        Glide.with(this).load(R.mipmap.mall1).into(iv_mall1);
+        Glide.with(this).load(R.mipmap.mall2).into(iv_mall2);
+        Glide.with(this).load(R.mipmap.mall3).into(iv_mall3);
+        Glide.with(this).load(R.mipmap.store1).into(iv_store1);
+        Glide.with(this).load(R.mipmap.store2).into(iv_store2);
+        Glide.with(this).load(R.mipmap.store3).into(iv_store3);
+        Glide.with(this).load(R.mipmap.store4).into(iv_store4);
+        Glide.with(this).load(R.mipmap.store5).into(iv_store5);
+    }
 
 }
--- app/src/main/java/com/reggie/digitalwallet/Test/TestData.java
+++ app/src/main/java/com/reggie/digitalwallet/Test/TestData.java
@@ -91,10 +91,10 @@ public class TestData {
 
     public static List<Specialist> getSpecialistData(){
         List<Specialist> specialists = new ArrayList<>();
-        Specialist specialist1 = new Specialist("专家1","简介1", 10,"https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1593150165,3335242820&fm=27&gp=0.jpg");
-        Specialist specialist2 = new Specialist("专家1","简介1", 10,"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1520356663586&di=ddfcd1406aa59ef0c30be93b5f9a4270&imgtype=jpg&src=http%3A%2F%2Fimg4.imgtn.bdimg.com%2Fit%2Fu%3D2620668064%2C2727905968%26fm%3D214%26gp%3D0.jpg");
-        Specialist specialist3 = new Specialist("专家1","简介1", 10,"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1520356605937&di=894ecb70a667d29fd95cc4fe50426ab7&imgtype=0&src=http%3A%2F%2Fwsb.cdsu.edu.cn%2F_data%2F2013%2F10%2F16%2F5f25ff49_ea3c_49e5_bffb_5e7c8ad0a858%2Fimages%2F435192010162653_1.gif");
-        Specialist specialist4 = new Specialist("专家1","简介1", 10,"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1520356755794&di=b1e62c97c4096ce3f129dfbce1743439&imgtype=0&src=http%3A%2F%2Fwww.hprc.org.cn%2Fleidaxinxi%2Fgfjswj%2F201311%2FW020131114288079631737.jpg");
+        Specialist specialist1 = new Specialist("专家1","简介1", 10,"http://120.79.17.123/static/img/crowdfunding2.jpg");
+        Specialist specialist2 = new Specialist("专家1","简介1", 10,"http://120.79.17.123/static/img/crowdfunding2.jpg");
+        Specialist specialist3 = new Specialist("专家1","简介1", 10,"http://120.79.17.123/static/img/crowdfunding2.jpg");
+        Specialist specialist4 = new Specialist("专家1","简介1", 10,"http://120.79.17.123/static/img/crowdfunding2.jpg");
 
         specialists.add(specialist1);
         specialists.add(specialist2);
